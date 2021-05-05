d3f8706245285c01273b3d8382916dfe09961b18
Tristan Germer <tgermer@me.com>
2021-05-05 13:03:32 +0200

    Anpassungen

---
+++ assets/js/own/navCollaps.js
@@ -0,0 +1,4 @@
+// Navbar collaps by clicking on a link
+// $('.navbar-nav>li>a').on('click', function() {
+//   $('.navbar-collapse').collapse('hide');
+// })
---
+++ assets/js/own/navVersatz.js
@@ -0,0 +1,10 @@
+// Hier komme ich noch nicht auf das entsprechene Element
+
+// var divId;
+//
+// $('.toc-entry').click(function(){
+// divId = $(this).attr('href');
+//  $('html, body').animate({
+//   scrollTop: $(divId).offset().top - 200
+// }, 500);
+// });
